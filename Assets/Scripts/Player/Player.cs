3c7c6c4ced2a7ceec3cd62129c90158c773db41d
burakbingoltr <burakbingoltr@gmail.com>
2024-10-07 02:11:57 +0300

    Updated Player's Facing Direction

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-  [SerializeReference]  private float moveSpeed = 7.5f;
+  [SerializeField]  private float moveSpeed = 7.5f;
+  [SerializeField] private GameObject rightLeg;
+  [SerializeField] private GameObject leftLeg;
+    [HideInInspector] private bool isFacingRight;
   private Rigidbody2D rb;
   private float moveInput;
   
   private void Start() {
     rb = GetComponent<Rigidbody2D>();
+    startDirectionCheck();
   }
 
   private void Update() {
@@ -20,5 +24,51 @@ public class Player : MonoBehaviour
   private void Move(){
     moveInput = UserInput.instance.moveInput.x;
     rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+    if(moveInput > 0 || moveInput < 0)
+        {
+            turnCheck();
+        }
   }
+
+    private void startDirectionCheck()
+    {
+        if (rightLeg.transform.position.x > leftLeg.transform.position.x)
+        {
+            isFacingRight = true;
+        }
+        else
+        {
+            isFacingRight = false;
+        }
+    }
+
+    private void turnCheck()
+    {
+        if(UserInput.instance.moveInput.x > 0 && !isFacingRight)
+        {
+            Turn();
+        }
+        else if(UserInput.instance.moveInput.x < 0 && isFacingRight)
+        {
+            Turn();
+        }
+    }
+
+    private void Turn()
+    {
+        if (isFacingRight)
+        {
+            Vector3 rotator = new Vector3(transform.position.x, 180f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight= !isFacingRight;
+        }
+        else
+        {
+            Vector3 rotator = new Vector3(transform.position.x, 0f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight = !isFacingRight;
+        }
+    }
+
 }
